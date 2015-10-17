d34131adabe02150de0bc0436c676adfa2ff93a1
oriken111 <oriken111@gmail.com>
2015-10-18 06:12:15 +0900

    Generated StaticPages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(sub_title)
+    base_title = 'Sample App from Rails Tutorial'
+    sub_title.empty? ? base_title : sub_title + ' | ' + base_title
+  end
 end
